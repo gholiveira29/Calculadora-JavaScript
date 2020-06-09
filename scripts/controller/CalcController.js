27f1ecc8c8c55e86bf2e890118d84e15121f9339
guilherme <gpoliveira@rd.com.br>
2020-06-09 15:10:57 -0300

    adicionando metodos de click

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,30 +1,84 @@
 class CalcController {
 
     constructor() {
-        this._displayCalc = '0';
+        this._locale = 'pt-BR';
+        this._displayCalcEl = document.querySelector('#display');
+        this._dateEl = document.querySelector('#data');
+        this._timeEl = document.querySelector('#hora');
         this._currentDate;
         this.initialize();
+        this.initButtonsEvents();
     }
 
     initialize() {
 
-        let displayCalcEl = document.querySelector('#display');
-        let dateEl = document.querySelector('#data');
-        let timeEl = document.querySelector('#hora');
+        this.setDisplayDateTime();
 
-        displayCalcEl.innerHTML ='0'
-        dateEl.innerHTML
-        timeEl.innerHTML
+        setInterval(()=>{
+
+            this.setDisplayDateTime();
+            
+        }, 1000 );
+
+    }
+
+    addEventListenerAll(elements, events, fn){
+
+        events.split(' ').forEach(event => {
+
+            elements.addEventListener(event, fn,false);
+        });
+
+    }
+
+    initButtonsEvents() {
+        let buttons = document.querySelectorAll('#buttons > g, #parts > g');
+
+        buttons.forEach((btn, index )=>{
+
+            this.addEventListenerAll(btn,'click drag', e =>{
+                console.log();
+            });
+
+            this.addEventListenerAll(btn, 'mouseover mouseup mousedown', e => {
+
+                btn.style.cursor = 'pointer'
+            });
+
+        });
+    }
+
+    setDisplayDateTime() {
+
+        this.displayDate = this.currentDate.toLocaleDateString(this._locale)
+            this.displayTime = this.currentDate.toLocaleTimeString(this._locale)
+
+    }
+
+    get displayTime() {
+        return this._timeEl.innerHTML;
+    }
+
+    set displayTime (value) {
+        return this._timeEl.innerHTML = value;
+    }
+
+    get displayDate() {
+        return this._dateEl.innerHTML;
+    }
+
+    set displayDate(value) {
+        return this._dateEl.innerHTML = value;
     }
 
     get displayCalc() {
-        return this._displayCalc;
+        return this._displayCalcEl.innerHTML;
     }
     set displayCalc(value) {
-        this._displayCalc = value;
+        this._displayCalcEl.innerHTML = value;
     }
     get currentDate(){
-        return this._currentDate;
+        return new Date();
     }
     set currentDate(value) {
         this._currentDate = value;
