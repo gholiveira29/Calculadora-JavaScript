af66eb0fb6847a482b1b19403ebe097f90ea7529
guilherme <gpoliveira@rd.com.br>
2020-06-10 15:54:54 -0300

    adicionando as funcionalidades

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,6 +1,7 @@
 class CalcController {
 
     constructor() {
+        this._operation = [];
         this._locale = 'pt-BR';
         this._displayCalcEl = document.querySelector('#display');
         this._dateEl = document.querySelector('#data');
@@ -31,13 +32,155 @@ class CalcController {
 
     }
 
+    clearAll(){
+
+        this._operation = [];
+    }
+
+    clearEntry() {
+        this._operation.pop();
+    }
+
+    setError(){
+        this.displayCalc = 'Error';
+    }
+
+    setLastOperation(value) {
+        this._operation[this._operation.length - 1] = value;
+    }
+
+    pushOperation(value) {
+        this._operation.push(value);
+
+        if(this._operation.length > 3) {
+            this.calc();
+        }
+    }
+
+    calc() {
+        let last = this._operation.pop();
+
+        let result = eval(this._operation.join(''));
+
+        this._operation = [result, last];
+
+        this.setLastNumberToDisplay();
+    }
+
+    getLastOperation() {
+        return this._operation[this._operation.length-1];
+    }
+
+    isOperation(value){
+        return (['+', '-' ,'*', '/', '%'].indexOf(value) > -1);
+    }
+
+    setLastNumberToDisplay() {
+
+        let lastNumber;
+
+        for(let  i = this._operation.length-1; i >= 0 ; i--) {
+            if (!this.isOperation(this._operation[i])) {
+            lastNumber = this._operation[i];
+            break;
+        }
+    }
+
+    this.displayCalc = lastNumber;
+
+    }
+
+
+    addOperation(value) {
+        
+        if(isNaN(this.getLastOperation())) {
+            if(this.isOperation(value)) {
+
+                this.setLastOperation(value);
+
+            }else if(isNaN(value)) {
+                console.log('aqui', value)
+
+            } else {
+                this.pushOperation(value);
+                this.setLastNumberToDisplay();
+            }
+        }else {
+
+            if (this.isOperation(value)) {
+
+                this.pushOperation(value);
+
+            }else {
+
+                let newValue = this.getLastOperation().toString() + value.toString();
+                this.setLastOperation(parseInt(newValue));
+
+                this.setLastNumberToDisplay();
+            }
+
+        }
+
+    }
+
+
+    execBtn(value) {
+        switch (value) {
+            case 'ac':
+                this.clearAll();
+                break;
+            case 'ce':
+                this.clearEntry();
+                break;
+            case 'soma':
+                this.addOperation('+');
+                break;
+            case 'subtracao':
+                this.addOperation('-');
+                break;
+            case 'divisao':
+                this.addOperation('/');
+                break;
+            case 'multiplicacao':
+                this.addOperation('*');
+                break;
+            case 'porcento':
+                this.addOperation('%');
+                break;
+            case 'igual':
+
+                break;
+            case 'ponto':
+                this.addOperation('.');
+                break;
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                this.addOperation(parseInt(value));
+                break;
+
+            default:
+                this.setError()
+                break;
+        }
+    }
+
     initButtonsEvents() {
         let buttons = document.querySelectorAll('#buttons > g, #parts > g');
 
         buttons.forEach((btn, index )=>{
 
             this.addEventListenerAll(btn,'click drag', e =>{
-                console.log();
+                let textBtn = btn.className.baseVal.replace('btn-', '');
+
+                this.execBtn(textBtn);
             });
 
             this.addEventListenerAll(btn, 'mouseover mouseup mousedown', e => {
